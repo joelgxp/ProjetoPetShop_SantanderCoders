1276ece65d3cd9b70a6d2dfb1bfa78f178559dd3
Joel <joelvieirasouza@gmail.com>
2023-01-22 19:06:55 -0300

    correcao metodo pagamento classe petshop

--- src/br/ada/petshop/Main.java
+++ src/br/ada/petshop/Main.java
@@ -23,14 +23,10 @@ public class Main {
         Gato animal03 = new Gato("Dexter", LocalDate.of(2014,8,10), "Persa", Porte.PEQUENO,5.0, EstadoAnimal.SUJO, null,null);
         Gato animal04 = new Gato("Conan", LocalDate.of(2017, 12, 10), "Siamês", Porte.PEQUENO,12.0, EstadoAnimal.SUJO, null, null);
 
-        List<Animais> listaAnimais01 = new ArrayList<>();
-        List<Animais> listaAnimais02 = new ArrayList<>();
-        listaAnimais01.addAll(Arrays.asList(animal01));
-        listaAnimais02.addAll(Arrays.asList(animal02, animal03));
-
-
-
         //ATENDIMENTO PARA 1 PET
+        List<Animais> listaAnimais01 = new ArrayList<>();
+        listaAnimais01.addAll(Arrays.asList(animal01));
+
         System.out.println("**CLIENTE COM 1 PET************************************************************");
         Cliente cliente01 = new Cliente(1, listaAnimais01);
         cliente01.setNome("Joel");
@@ -53,11 +49,17 @@ public class Main {
         listItens.add(atendimentoClinico.getId());
         listItens.add(vacinacao.getId());
         listItens.add(higienizacao.getId());
+        listItens.add(13);
+        listItens.add(23);
 
         petshop.pagamentos(listItens);
 
         //ATENDIMENTO PARA 2 PETS
+        System.out.println();
         System.out.println("**CLIENTE COM 2 PET***********************************************************");
+
+        List<Animais> listaAnimais02 = new ArrayList<>();
+        listaAnimais02.addAll(Arrays.asList(animal02, animal03));
 
         Cliente cliente02 = new Cliente(2, listaAnimais02);
         cliente02.setNome("Samuel");
@@ -66,32 +68,32 @@ public class Main {
         System.out.println(atendimentoClinico);
 
         List<Vacinas> vacinasList2 = new ArrayList<>();
-        for (int i = 0; i < atendimentoClinico.getCliente().getPets().size(); i++) {
-            vacinasList.add(Vacinas.valueOf(atendimentoClinico.getCliente().getPets().get(i).getObservacoes()));
+        for (int i = 0; i < atendimentoClinico2.getCliente().getPets().size(); i++) {
+            vacinasList.add(Vacinas.valueOf(atendimentoClinico2.getCliente().getPets().get(i).getObservacoes()));
         }
 
         ResponseVO vacinacao2 = petshop.vacinacao(cliente02, cliente02.getPets(), vacinasList, null);
-        System.out.println(vacinacao);
+        System.out.println(vacinacao2);
 
         ResponseVO higienizacao2 = petshop.higienizar(cliente02, cliente02.getPets(), Higiene.BANHO_E_TOSA,"Cortar as unhas");
-        System.out.println(higienizacao);
+        System.out.println(higienizacao2);
 
         List<Integer> listItens2 = new ArrayList<>();
         listItens2.add(atendimentoClinico.getId());
         listItens2.add(vacinacao.getId());
         listItens2.add(higienizacao.getId());
+        listItens2.add(11);
+        listItens2.add(12);
+        listItens2.add(21);
+        listItens2.add(22);
 
-        petshop.pagamentos(listItens);
+        petshop.pagamentos(listItens2);
 
-
+        System.out.println();
         System.out.println("**LISTA DE ALIMENTOS************************************************************");
         petshop.verAlimentos();
         System.out.println("**LISTA DE REMEDIOS************************************************************");
-        petshop.verAlimentos();
-        System.out.println("**PAGAMENTOS************************************************************");
-        petshop.pagamentos(listItens);
-
-
+        petshop.verRemedios();
 
     }
 }
--- src/br/ada/petshop/services/Petshop.java
+++ src/br/ada/petshop/services/Petshop.java
@@ -26,23 +26,19 @@ public class Petshop {
     }
 
     private static List<Remedios> criarListaRemedios() {
-        var remedio01 =  new Remedios(11, "Medicamento Anti-inflamatório Dermatite Cães/gatos", new BigDecimal("45.40"));
-        var remedio02 =  new Remedios(12, "Medicamento Corticoide, Sarnas,dermatite Cães E Gatos", new BigDecimal("59.35"));
+        var remedio01 =  new Remedios(11, "Medicamento Anti-inflamatório Dermatite Cães/gatos", new BigDecimal("45.50"));
+        var remedio02 =  new Remedios(12, "Medicamento Corticoide, Sarnas,dermatite Cães E Gatos", new BigDecimal("59.90"));
         var remedio03 =  new Remedios(13, "Anti Inflamatório Dexagard Para Cães E Gatos", new BigDecimal("29.90"));
-        var remedio04 =  new Remedios(14, "Calmante Natural Para Caes E Gatos 30 Cápsulas Nicpharma", new BigDecimal("18.50"));
-        var remedio05 =  new Remedios(15, "Antibiótico Ourofino Celesporin 12 Comprimidos", new BigDecimal("46.90"));
 
-        return Arrays.asList(remedio01, remedio02, remedio03, remedio04, remedio05);
+        return Arrays.asList(remedio01, remedio02, remedio03);
     }
 
     private static List<Alimentos> criarListaAlimentos() {
         var alimento01 =  new Alimentos(21, "Comida Natural para Cachorro sabor Carne", new BigDecimal("89.90"));
-        var alimento02 =  new Alimentos(22, "Alimento para Gatos Bancat 25kg", new BigDecimal("73.50"));
-        var alimento03 =  new Alimentos(23, "Pet Protein Proteico Em Pó Cães Gato", new BigDecimal("59.90"));
-        var alimento04 =  new Alimentos(24, "Papinha Gato (gatos filhotes)", new BigDecimal("8.39"));
-        var alimento05 =  new Alimentos(25, "Petisco Comida Natural Para Cachorros E Gatos", new BigDecimal("58.0"));
+        var alimento02 =  new Alimentos(22, "Alimento para Gatos Bancat 25kg", new BigDecimal("79.90"));
+        var alimento03 =  new Alimentos(23, "Pet Protein Proteico Em Pó Cães Gato", new BigDecimal("69.90"));
 
-        return Arrays.asList(alimento01, alimento02, alimento03, alimento04, alimento05);
+        return Arrays.asList(alimento01, alimento02, alimento03);
     }
 
 
@@ -95,7 +91,8 @@ public class Petshop {
             esquemaVacinalList.add(new EsquemaVacinal(LocalDate.of(2022,8,2),vacinas.get(i),"Vacinado"));
             animais.get(i).setVacinas(esquemaVacinalList);
         }
-        return new ResponseVO(VACINAR, Servicos.VACINACAO, new BigDecimal("80.0"), cliente);
+        BigDecimal valor = BigDecimal.valueOf(animais.size());
+        return new ResponseVO(VACINAR, Servicos.VACINACAO, new BigDecimal("80.0").multiply(valor), cliente);
     }
 
     public void verAlimentos(){
@@ -116,19 +113,26 @@ public class Petshop {
                 valorServ += 300.0;
             }else if (itens.get(i) == 3) {
                 valorServ += 80.0;
-            }else if (itens.get(i) == 4) {
-                valorProd = 59.90;
-            }else if (itens.get(i) == 5) {
-                valorProd = 39.90;
-            }else if (itens.get(i) == 6) {
-                valorProd = 29.90;
+            }else if (itens.get(i) == 11) {
+                valorProd += 45.50;
+            }else if (itens.get(i) == 12) {
+                valorProd += 59.90;
+            }else if (itens.get(i) == 13) {
+                valorProd += 29.90;
+            }else if (itens.get(i) == 21) {
+                valorProd += 89.90;
+            }else if (itens.get(i) == 22) {
+                valorProd += 79.90;
+            }else if (itens.get(i) == 23) {
+                valorProd += 69.90;
 
             }
         }
-        System.out.printf("******* COMPROVATE DE PAGAMENTO ******");
-        System.out.println();
-        System.out.println("Valor total em servicos: " + valorServ);
-        System.out.println("Valor total em produtos: " + valorProd);
+        //Double valor = atendimentoClinico().getCliente().getPets().size();
+        System.out.println("******* COMPROVATE DE PAGAMENTO ******");
+        System.out.printf("Valor total em servicos: %.2f %n", valorServ);
+        System.out.printf("Valor total em produtos: %.2f %n", valorProd);
+        System.out.printf("Valor total a pagar: %.2f %n", valorProd + valorServ);
 
 
     }
